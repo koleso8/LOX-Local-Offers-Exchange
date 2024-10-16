f9bd3c38a4e5ab3e0f19dd53ecb96af59aa73043
koleso8 <dev.koleso@gmail.com>
2024-10-16 10:21:37 +0300

    add hovers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,10 @@ const Header = () => {
   return (
     <div className="w-full bg-slate-950 h-[120px] py-6 fixed z-50">
       <div className="w-4/5 flex justify-between mx-auto my-auto items-center">
-        <Link to="/" className="flex flex-col h-20 text-white">
+        <Link
+          to="/"
+          className="flex flex-col h-20 text-white  hover:[text-shadow:#FFF_0px_0px_10px,_#FFF_0px_0px_15px,_0px_0px_3px_rgba(206,89,55,0)] transition-all ease-linear"
+        >
           <h3 className="text-[40px] font-black h-12 ">LOX</h3>
           <p className="text-xl  h-6">Local Offers Exchange</p>
         </Link>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,17 +2,29 @@ import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
   return (
-    <nav className="text-white text-l font-semibold flex gap-4">
-      <NavLink to="/" className="py-[10px] px-[15px]">
+    <nav className="text-white text-l font-semibold flex gap-4 ">
+      <NavLink
+        to="/"
+        className="py-[10px] px-[15px]  hover:[text-shadow:#FFF_0px_0px_10px,_#FFF_0px_0px_15px,_0px_0px_3px_rgba(206,89,55,0)] transition-all ease-linear"
+      >
         Головна
       </NavLink>
-      <NavLink to="about" className="py-[10px] px-[15px]">
+      <NavLink
+        to="about"
+        className="py-[10px] px-[15px]  hover:[text-shadow:#FFF_0px_0px_10px,_#FFF_0px_0px_15px,_0px_0px_3px_rgba(206,89,55,0)] transition-all ease-linear"
+      >
         Про нас
       </NavLink>
-      <NavLink to="announcement" className="py-[10px] px-[15px]">
+      <NavLink
+        to="announcement"
+        className="py-[10px] px-[15px] hover:[text-shadow:#FFF_0px_0px_10px,_#FFF_0px_0px_15px,_0px_0px_3px_rgba(206,89,55,0)] transition-all ease-linear"
+      >
         Оголошення
       </NavLink>
-      <NavLink to="lawyers" className="py-[10px] px-[15px]">
+      <NavLink
+        to="lawyers"
+        className="py-[10px] px-[15px] hover:[text-shadow:#FFF_0px_0px_10px,_#FFF_0px_0px_15px,_0px_0px_3px_rgba(206,89,55,0)] transition-all ease-linear"
+      >
         Юристи
       </NavLink>
     </nav>
