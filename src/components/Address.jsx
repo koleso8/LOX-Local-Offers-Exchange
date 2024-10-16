23ce32859af0f046fe0df4f77489517bb46f93b5
koleso8 <dev.koleso@gmail.com>
2024-10-16 09:58:33 +0300

    small fix

--- src/components/Address.jsx
+++ src/components/Address.jsx
@@ -9,7 +9,7 @@ const Address = () => {
         <a href="tel:+380671358585">+38(067)135-85-85</a>
         <a href="mailto:lox@gmail.com">lox@gmail.com</a>
       </address>
-      <ul className="flex gap-5 mb-8">
+      <ul className="flex gap-5 mb-6">
         <li className=" p-1 border-black border-[1px] rounded-full">
           <a className="" href="" target="_blank">
             <FaTelegram size="20px" />
