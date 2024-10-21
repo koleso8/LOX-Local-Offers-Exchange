9e608bfab61f234aeb4a264e2274dc34feae0ec4
koleso8 <dev.koleso@gmail.com>
2024-10-21 17:39:46 +0300

    Update AnnouncementsPage.jsx

--- src/pages/AnnouncementsPage.jsx
+++ src/pages/AnnouncementsPage.jsx
@@ -1,7 +1,10 @@
 import { announcements } from '../test/announcements';
 import AnnouncemenList from '../components/AnnouncementList';
+import { useSelector } from 'react-redux';
+import { selectAnnouncements } from '../redux/announcements/selector';
 
 const AnnouncementsPage = () => {
+  const announ = useSelector(selectAnnouncements);
   return (
     <div className="text-[100px] text-black py-2 w-full">
       <AnnouncemenList array={announcements} />
